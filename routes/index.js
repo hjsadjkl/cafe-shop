e4c8d85b7fcc34c3d2ca84da63785d99f19e1bd3
hjsadjkl <90150470+hjsadjkl@users.noreply.github.com>
2022-11-21 01:15:04 +0800

    ilica

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const CaseName = "CaseName"
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'coffee' });
+  res.render('index', {
+    title: CaseName, 
+    TestList: '<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>QQ'
+
+  });
 });
+
+
 
 module.exports = router;
