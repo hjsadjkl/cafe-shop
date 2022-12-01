f82c48e72021b143724ffb93bbc36baf386e16e8
hjsadjkl <90150470+hjsadjkl@users.noreply.github.com>
2022-12-01 10:42:01 +0800

    ilica

--- routes/search.js
+++ routes/search.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql2');
-var dotenv = require('dotenv');
+const config = require('dotenv').config()
 
 const pool =  mysql.createPool({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'hjsadjkl' ,
-  database: 'webwork'
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE
 }).promise()
 
 async function getTable(){
@@ -18,13 +18,12 @@ async function getTableId(id){
   const [rows] = await pool.query(`SELECT ? FROM Article`,id);
 }
 
-
 router.get('/', async function(req, res, next){
   let database = await getTable("title");
   let title = [];
   for(let i=0; i<database.length; i++){
     title.push(database[i].title);
-    
+
   }
   res.render('search',{
     item: title,
